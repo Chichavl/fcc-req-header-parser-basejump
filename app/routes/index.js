d45585bc9d56d043ca3547966359dec3018cde7e
Vadim Grigorov <chichavl@gmail.com>
2016-04-03 02:04:28 +0000

    final solution

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,9 +9,14 @@ module.exports = function (app) {
 			res.sendFile(path + '/public/index.html');
 		});
 
-	// app.route('/login')
-	// 	.get(function (req, res) {
-	// 		res.sendFile(path + '/public/login.html');
-	// 	});
+	app.route('/api/whoami')
+		.get(function (req, res) {
+			var obj = {};
+			obj.ipaddress = req.headers["x-forwarded-for"];
+			obj.language = req.headers["accept-language"].split(",")[0];
+			obj.software = req.headers["user-agent"].split(") ")[0].split(" (")[1];
+			
+			res.end(JSON.stringify(obj));
+		});
 
 };
